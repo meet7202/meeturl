3f9757fc1dbee9674f6490ff7487eb9417ec1922
Meet Shah <meetshah7202@gmail.com>
2018-06-11 23:01:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ var config = {};
 
 config.db = {};
 config.webhost = 'http://meeturl.herrokuapp.com';
+config.port = process.env.PORT || 5000;
 
 //config.db.host = 'localhost';
 //config.db.name = 'url_shortener';
